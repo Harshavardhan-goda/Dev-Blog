0f090db2766df0b87f7747fe653af0321c4447d7
harsha <harshavardhangoda27@gmail.com>
2023-05-12 15:43:35 +0000

    create fetch by using of React Routers

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,45 @@
+import Loader from 'react-loader-spinner'
+import {Component} from 'react'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogData()
+  }
+
+  getBlogData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updateData = data.map(each => ({
+      id: each.id,
+      title: each.title,
+      imageUrl: each.image_url,
+      avatarUrl: each.avatar_url,
+      topic: each.topic,
+    }))
+
+    this.setState({blogsData: updateData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
